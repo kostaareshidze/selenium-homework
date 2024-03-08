670dbf4de879b7c83dd0428d8be918ff58d909fc
user2 <kostaareshide@gmail.com>
2024-03-08 18:36:50 +0400

    Added: webElements

--- src/main/java/ge/tbcacademy/data/Constants.java
+++ src/main/java/ge/tbcacademy/data/Constants.java
@@ -1,11 +1,19 @@
 package ge.tbcacademy.data;
 
 public class Constants {
-    public static String firstLink = "http://the-internet.herokuapp.com/dynamic_controls";
-    public static String secondLink = "http://the-internet.herokuapp.com/drag_and_drop";
-    public static String thirdLink = "https://ultimateqa.com/automation";
-    public static String testimonialURL = "https://ultimateqa.com/testimonials/";
-    public static String selenium = "http://elementalselenium.com/";
-    public static String description = "This example demonstrates when elements " +
+    public static final String firstLink = "http://the-internet.herokuapp.com/dynamic_controls";
+    public static final String secondLink = "http://the-internet.herokuapp.com/drag_and_drop";
+    public static final String thirdLink = "https://ultimateqa.com/automation";
+    public static final String testimonialURL = "https://ultimateqa.com/testimonials/";
+    public static final String selenium = "http://elementalselenium.com/";
+    public static final String description = "This example demonstrates when elements " +
             "(e.g., checkbox, input field, etc.) are changed asynchronously.";
+    public static final String jqueryuiURl = "https://jqueryui.com/slider/";
+    public static final String buttonDriverURL = "http://the-internet.herokuapp.com/add_remove_elements/";
+    public static final String challengingURL = "http://the-internet.herokuapp.com/challenging_dom";
+    public static final String relativeXpath = "/html/body/div[2]/div/div/div/div/div[2]/table/tbody/tr[10]/td[contains(text(),'Apeirian9')]/preceding-sibling::td[contains(text(),'Iuvaret')]";
+    public static final String path = "//aside/h3[text()='Effects' and @class='widget-title']/parent::*/ul";
+    public static final String deletePath = "//div[@id='elements']/button[count(following-sibling::button) = 0]";
+    public static final String css = "div[id^='elem']";
+    public static final String manual = "added-manually";
 }
