d04da22e6ee2bd9ecedaad9f16c7bcf32df99918
user2 <kostaareshide@gmail.com>
2024-03-11 20:03:03 +0400

    Forms and waits

--- src/main/java/ge/tbcacademy/data/Constants.java
+++ src/main/java/ge/tbcacademy/data/Constants.java
@@ -16,4 +16,21 @@ public class Constants {
     public static final String deletePath = "//div[@id='elements']/button[count(following-sibling::button) = 0]";
     public static final String css = "div[id^='elem']";
     public static final String manual = "added-manually";
+    public static final String tympanusURL = "https://tympanus.net/Tutorials/CustomDropDownListStyling/";
+    public static final String canvaURL = "https://techcanvass.com/examples/register.html";
+    public static final String internetURL = "http://the-internet.herokuapp.com/dynamic_controls";
+    public static final String democaURL = "https://demoqa.com/progress-bar";
+    public static final String dropDown = "wrapper-dropdown-2";
+    public static final String dropDownActive = "wrapper-dropdown-2 active";
+    public static final String mega = "Mega 123 Medium screen";
+    public static final String address1 = "Abashidz str 6";
+    public static final String address2 = "Gulua str 5";
+    public static final String access = "ACCESS GRANTED";
+    public static final String maxValue = "100%";
+    public static final String disable = "Disable";
+    public static final String number1 = "599765677";
+    public static final String number2 = "571887766";
+    public static final String name = "Konstantine";
+    public static final String lastName = "Areshidze";
+    public static final String location = "Tbilisi";
 }
